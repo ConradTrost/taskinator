adf79a0c206da9cd9a1e8adfe87bbcec39fde6bc
Conrad Trost <ctrost@tamu.edu>
2020-04-09 18:20:24 -0500

    New task funciton created on button click

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var buttonEL = document.querySelector("#save-task");
+var tasksToDoEL = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEL = document.createElement("li");
+    listItemEL.className = "task-item";
+    listItemEL.textContent = "This is a new task.";
+    tasksToDoEL.appendChild(listItemEL);
+}
+
+buttonEL.addEventListener("click", createTaskHandler);
