5ddb24dd76b76a2f8b14d7bffc7d175330c5b20c
Conrad Trost <ctrost@tamu.edu>
2020-04-11 13:05:40 -0500

    seperated funcitons in js for form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
-var buttonEL = document.querySelector("#save-task");
+var formEL = document.querySelector("#task-form");
 var tasksToDoEL = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var taskFormHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEL(taskDataObj);
+}
+
+var createTaskEL = function(taskDataObj) {
     var listItemEL = document.createElement("li");
     listItemEL.className = "task-item";
-    listItemEL.textContent = "This is a new task.";
+
+    var taskInfoEL = document.createElement("div");
+    taskInfoEL.className = "task-info";
+    taskInfoEL.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEL.appendChild(taskInfoEL);
     tasksToDoEL.appendChild(listItemEL);
 }
 
-buttonEL.addEventListener("click", createTaskHandler);
+formEL.addEventListener("submit", taskFormHandler);
